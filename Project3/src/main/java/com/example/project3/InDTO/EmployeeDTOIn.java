02d550de6fb4126a676eef4305b19f608d906dd1
Raghad-419 <143605062+Raghad-419@users.noreply.github.com>
2024-12-25 07:15:23 +0300

    Add files via upload

--- Project3/src/main/java/com/example/project3/InDTO/EmployeeDTOIn.java
+++ Project3/src/main/java/com/example/project3/InDTO/EmployeeDTOIn.java
@@ -12,21 +12,21 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class EmployeeDTOIn {
-//    @NotEmpty(message = "Empty name")
-//    @Size(min = 4,max = 10,message = "Length of username must be between 4 and 10 characters")
+    @NotEmpty(message = "Empty name")
+    @Size(min = 4,max = 10,message = "Length of username must be between 4 and 10 characters")
     private String username;
-//    @NotEmpty(message = "Empty password")
-//    @Size(min = 6,message = "Password must be at least 6 characters")
+    @NotEmpty(message = "Empty password")
+    @Size(min = 6,message = "Password must be at least 6 characters")
     private String password;
-//    @NotEmpty(message = "Empty name")
-//    @Size(min = 2,max = 20,message = "Name length must be between 2 and 20 characters")
+    @NotEmpty(message = "Empty name")
+    @Size(min = 2,max = 20,message = "Name length must be between 2 and 20 characters")
     private String name;
-//    @Email
-//    @NotEmpty(message = "Empty email")
+    @Email
+    @NotEmpty(message = "Empty email")
     private String email;
-   // @NotEmpty(message = "Empty position")
+    @NotEmpty(message = "Empty position")
     private String position;
-//    @PositiveOrZero(message = "Salary must ne positive")
-//    @NotNull(message = "Empty salary")
+    @PositiveOrZero(message = "Salary must ne positive")
+    @NotNull(message = "Empty salary")
     private Double salary;
 }
